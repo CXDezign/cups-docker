e1fc6998050bd7aa004251a660a8794e48aa810d
CXDezign <29733018+CXDezign@users.noreply.github.com>
2025-12-13 14:29:26 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,6 @@
 # Environment Variables
 set -e
 
-# PPDs
-mv ./ppd/cnijfilter2_6.80-1_${TARGET_ARCH}.deb /tmp/cnijfilter2.deb
-apt install -y /tmp/cnijfilter2.deb
-
 # Default Configuration
 /usr/sbin/cupsd \
   && while [ ! -f /var/run/cups/cupsd.pid ]; do sleep 1; done \
